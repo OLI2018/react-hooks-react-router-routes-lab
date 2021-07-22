fb04c93604a601aeaef4299ff59f15f882abf644
Oleg <roxvemm2014@gmail.com>
2021-07-21 23:46:34 -0700

    initial commit

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const listOfDir = directors.map((director) => (
+  <div key = {director.name}>
+    <h2>Name: {director.name}</h2>
+    <p>Moveis:</p>
+    <ul>
+      {director.movies.map((movie) => (
+      <li key = {movie}>{movie}</li>
+      ))}
+    </ul>
+  </div>
+  ));
+
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {listOfDir}
+  </div>
+    )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((element) => (
+    <div key={element.title}>
+      <h2>Name: {element.title}</h2>
+      <p>Time: {element.time} min.</p>
+      <p>Genres:</p>
+      <ul>
+        {element.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
+
